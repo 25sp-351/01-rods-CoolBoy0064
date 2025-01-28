c9cdb1fdde38a79b0cf63f4ccc30775db37cb9e3
Liam Knight <70240755+CoolBoy0064@users.noreply.github.com>
2025-01-27 18:45:14 -0800

    Add files via upload

--- rod_cut.c
+++ rod_cut.c
@@ -12,7 +12,9 @@ int cut_rod(int values[], int supplied_rod_length, int *remainder){
     //Recursively find and compare every way the rods can be cut to find the highest value
     for (int i = 0; i < supplied_rod_length; i++){
         max_val = find_max(max_val, values[i] + cut_rod(values, supplied_rod_length - i - 1, remainder));
-        printf("%d @ %d = %d \n", supplied_rod_length, i, values[i]);
+        if(values[i] != 0){
+            printf("%d @ %d = %d \n", supplied_rod_length, i, values[i]);
+        }
         if (max_val > last_val){
             *remainder = supplied_rod_length - 1;
             last_val = max_val;
@@ -26,7 +28,7 @@ int main(){
     int supplied_rod_length;
     int *values; //The list of values of possible cut lengths
     int output_value;
-    int remainder; //the amount of length we will have remaining after the cutting is finished
+    int remainder = 0; //the amount of length we will have remaining after the cutting is finished
     int rod_list_length; //Inputted length of a rod
     int rod_list_value; //Inputted value of the same rod
 
@@ -46,7 +48,7 @@ int main(){
     }
 
     while(scanf("%d %d", &rod_list_length, &rod_list_value) != EOF){ //IF the user types a 'ctrl-z' the output fo scanf will be EOF
-        if(rod_list_length <= supplied_rod_length){ //If the user gives us a rod BIGGER than the supplied rod, we can never use this value and throw it out
+        if(rod_list_length <= supplied_rod_length && rod_list_length > 0){ //If the user gives us a rod BIGGER than the supplied rod, we can never use this value and throw it out
             values[rod_list_length - 1] = rod_list_value;
         }
     }
